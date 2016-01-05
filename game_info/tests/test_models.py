03180627bc48c4a5bd347cf6479d2b2c3b3e81b6
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-05 23:40:28 +0000

    Better unit tests using Mock

--- game_info/tests/test_models.py
+++ game_info/tests/test_models.py
@@ -1,13 +1,74 @@
+import mock
+import random
 from django.test import TestCase
 from game_info.models import Server, Player, Rule
+from valve.source import messages, util, a2s
 
-SERVER_DOWN_URL = "example.org"
-SERVER_UP_URL = "Surf.TF2.Azelphur.com"
 
-# models test
+class MockServerQuerier():
+    def __init__(self, address):
+        pass
+
+    def get_info(self):
+        i = messages.InfoResponse()
+        i['response_type'] = 73
+        i['protocol'] = 17
+        i['server_name'] = u'A test server'
+        i['map'] = u'ctf_2fort'
+        i['folder'] = u'tf'
+        i['game'] = u'Team Fortress'
+        i['app_id'] = 440
+        i['player_count'] = 31
+        i['max_players'] = 31
+        i['bot_count'] = 0
+        i['server_type'] = util.ServerType(100)
+        i['platform'] = util.Platform(108)
+        i['password_protected'] = 0
+        i['vac_enabled'] = 1
+        i['version'] = u'3200676'
+        return i
+
+    def get_players(self):
+        players = messages.PlayersResponse()
+        players['player_count'] = 32
+        players['response_type'] = 68
+        players['players'] = []
+        for i in range(1, 33):
+            player = messages.PlayerEntry()
+            player['index'] = 0
+            player['name'] = "Player %d" % (i)
+            player['score'] = random.randrange(1000)
+            player['duration'] = random.uniform(1, 10000)
+            players['players'].append(player)
+        return players
+
+    def get_rules(self):
+        r = messages.RulesResponse()
+        r['rule_count'] = 2
+        r['rules'] = {
+            'sv_alltalk': u'0',
+            'sv_voiceenable': u'1'
+        }
+        return r
+
+
+class MockServerDownQuerier():
+    def __init__(self, address):
+        pass
+
+    def get_info(self):
+        raise a2s.NoResponseError("Timed out waiting for response")
+
+    def get_players(self):
+        raise a2s.NoResponseError("Timed out waiting for response")
+
+    def get_rules(self):
+        raise a2s.NoResponseError("Timed out waiting for response")
+
+
 class ServerTest(TestCase):
 
-    def create_server(self, title="Test Server", host=SERVER_UP_URL, port=27015):
+    def create_server(self, title="Test Server", host="example.org", port=27015):
         return Server.objects.create(title=title, host=host, port=port)
 
     def create_player(self, server, name="Bob", score=100, duration=1000.0):
@@ -21,33 +82,39 @@ class ServerTest(TestCase):
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.__unicode__(), s.title)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerQuerier)
     def test_update_info(self):
         s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_info(), True)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerQuerier)
     def test_update_players(self):
         s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_players(), True)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerQuerier)
     def test_update_rules(self):
         s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_rules(), True)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerDownQuerier)
     def test_update_info_exception(self):
-        s = self.create_server(host=SERVER_DOWN_URL)
+        s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_info(), False)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerDownQuerier)
     def test_update_players_exception(self):
-        s = self.create_server(host=SERVER_DOWN_URL)
+        s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_players(), False)
 
+    @mock.patch('valve.source.a2s.ServerQuerier', MockServerDownQuerier)
     def test_update_rules_exception(self):
-        s = self.create_server(host=SERVER_DOWN_URL)
+        s = self.create_server()
         self.assertTrue(isinstance(s, Server))
         self.assertEqual(s.update_rules(), False)
 
@@ -62,4 +129,3 @@ class ServerTest(TestCase):
         r = self.create_rule(s)
         self.assertTrue(isinstance(r, Rule))
         self.assertEqual(r.__unicode__(), r.cvar)
-
