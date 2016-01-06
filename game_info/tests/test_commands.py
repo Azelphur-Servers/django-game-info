9014dfde50d5f54cf79c544ce01e81266effa87d
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-06 17:57:48 +0000

    Fix testing on update_game_info management command

--- game_info/tests/test_commands.py
+++ game_info/tests/test_commands.py
@@ -1,7 +1,12 @@
 from django.core.management import call_command
 from django.test import TestCase
+from game_info.models import Server
 
 
 class ServerTest(TestCase):
+    def create_server(self, title="Test Server", host="example.org", port=27015):
+        return Server.objects.create(title=title, host=host, port=port)
+
     def test_update_game_info(self):
+        self.create_server().save()
         call_command('update_game_info')
