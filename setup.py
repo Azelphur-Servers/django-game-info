b5a049b1b7b4c2d759aafc38ef2f9af148396bbd
Alfie "Azelphur" Day <support@azelphur.com>
2016-01-08 01:35:48 +0000

    Update do version 0.1.1 which should hopefully trigger automatic pypi deployment

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-game-info',
-    version='0.1',
+    version='0.1.1',
     packages=['game_info'],
     include_package_data=True,
     license='BSD License',
